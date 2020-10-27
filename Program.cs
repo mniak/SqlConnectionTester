35b8bc0f580b132d4b4136cc3ac67747de2d9840
Andre Soares <andregsilv@gmail.com>
2020-10-27 10:47:10 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace SqlConnectionTester
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var settings = new Settings();
+            config.Bind(settings);
+            settings.ThrowExceptionIfInvalid();
+
+            var logger = new LoggerConfiguration()
+               .WriteTo.Console(LogEventLevel.Verbose)
+               .CreateLogger();
+
+            while (true)
+            {
+                await PerformAsync(logger, settings.ConnectionString, settings.Query);
+                await Task.Delay(settings.Delay);
+            }
+        }
+
+        private static async Task PerformAsync(Logger logger, string connectionString, string query)
+        {
+            try
+            {
+                logger.Debug("Connecting...");
+                using var sqlConnection = new SqlConnection(connectionString);
+
+                logger.Debug("Executing SQL query: {Query}", query);
+                var result = await sqlConnection.ExecuteScalarAsync<int>(query);
+                logger.Information("Query result: {Result}", result);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Could not query database");
+            }
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SqlConnectionTester
+{
+    public class Settings
+    {
+        public string ConnectionString { get; set; }
+        public string TableName { get; set; }
+
+        private string query = "Person.Person";
+        public string Query
+        {
+            get => query ?? $"SELECT TOP(1) 1 FROM {TableName}";
+            set => query = value;
+        }
+
+        public int Delay { get; set; } = 2000;
+
+        public void ThrowExceptionIfInvalid()
+        {
+            if (ConnectionString == null)
+                throw new ArgumentNullException("Invalid connection string", nameof(ConnectionString));
+
+            if (Delay < 0)
+                throw new ArgumentOutOfRangeException("The delay must be positive", nameof(Delay));
+
+            if (Query == null)
+                throw new ArgumentNullException("Invalid query/table name", nameof(Query));
+        }
+    }
+}
